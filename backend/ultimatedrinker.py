ebaa4e1a97fde12d5771d6acb82095ce9d5dea0c
Bertware <bert.marcelis@outlook.com>
2015-07-17 19:17:18 +0200

    Hello world!

---
+++ backend/ultimatedrinker.py
@@ -0,0 +1,13 @@
+__author__ = 'bert'
+
+import cherrypy
+
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+
+if __name__ == '__main__':
+    cherrypy.quickstart(HelloWorld())
